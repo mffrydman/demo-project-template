8d41c31bea382a3e2660d0cdf8ba537ca2037739
mffrydman <78177534+mffrydman@users.noreply.github.com>
2025-05-03 16:59:42 -0300

    Testeo del commit

---
+++ src/dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David"],
+    "Age": [24, 27, 22, 32],
+    "City": ["New York", "LA", "Chicago", "Houston"],
+}
+
+df = pd.DataFrame(data)
+
+df.head()
